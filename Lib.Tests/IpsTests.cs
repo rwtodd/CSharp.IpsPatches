6dfe3ddd2600ecb0a46bc3b997a2bae808e0c8c5
Richard Todd <rwtodd@users.noreply.github.com>
2018-08-17 19:56:41 -0500

    removed unneeded 'using' statements.

--- Lib.Tests/IpsTests.cs
+++ Lib.Tests/IpsTests.cs
@@ -3,14 +3,12 @@ using System;
 using System.IO;
 using System.Threading.Tasks;
 using System.Linq;
-using System.Collections.Generic;
-using RWT.IpsPatch;
 
 
 namespace RWT.IpsPatch.Tests
 {
 
-	[TestClass]
+    [TestClass]
 	public class PatchTests
 	{
 
