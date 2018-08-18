de1090de95fcf07a23c15add82e0d44d5149d19c
Richard Todd <rwtodd@users.noreply.github.com>
2018-08-17 21:53:21 -0500

    Set up to use RWT.ArgParse from NuGet

--- Apply/Cmd.cs
+++ Apply/Cmd.cs
@@ -1,31 +1,55 @@
 using System;
-using System.Collections.Generic;
 using System.IO;
-using System.Linq;
-using System.Threading.Tasks;
 using RWT.IpsPatch;
+using RWT.ArgParse;
 
 namespace RWT.IpsApply
 {
 
-	class Cmd
+    class Cmd
 	{
 
 		public static void Main(String[] args)
 		{
+            String ipsFile = null, srcFile = null, resultFile = null;
 
-			if (args.Length != 3)
-			{
-				Console.Error.WriteLine("Usage: IpsApply <ipsfile> <srcfile> <resultfile>");
-				Environment.Exit(1);
-			}
-			var ipsFile = args[0];
-			var srcFile = args[1];
-			var resultFile = args[2];
-			Console.WriteLine($"Patching <{srcFile}> with <{ipsFile}> to produce <{resultFile}>");
+            var ap = new ArgParser(
+                new StrArg("-ips","<file> the IPS change file (REQUIRED)") {
+                    Command = (fn) => ipsFile = fn,
+                    Required = true
+                },
+                new StrArg("-src","<file> the original source (REQUIRED)")
+                {
+                    Command = (fn) => srcFile = fn,
+                    Required = true
+                },
+                new StrArg("-dest","<file> the destination output file (REQUIRED)")
+                {
+                    Command = (fn) => resultFile = fn,
+                    Required = true
+                },
+                new HelpArg("-help")
+                {
+                    Command = (opts) =>
+                    {
+                        Console.Error.WriteLine("Usage: IpsApply <options>");
+                        Console.Error.WriteLine();
+                        opts(Console.Error);
+                        Environment.Exit(1);
+                    }
+                });
 
-			try
-			{
+            try { 
+                var extras = ap.Parse(args);
+
+                if (extras.Count != 0)
+                {
+                    throw new ArgumentException("Unknown arguments given on the command line!");
+                }
+           
+
+    			Console.WriteLine($"Patching <{srcFile}> with <{ipsFile}> to produce <{resultFile}>");
+
 				int count = 1;
 				File.Copy(srcFile, resultFile);
 				using (FileStream ips = File.OpenRead(ipsFile),
@@ -40,6 +64,12 @@ namespace RWT.IpsApply
 						}).Wait();
 				}
 			}
+            catch (ArgParseException ape)
+            {
+                Console.Error.WriteLine(ape.Message);
+                Console.Error.WriteLine();
+                ap.ActivateSwitch("-help");
+            }
 			catch (Exception e)
 			{
 				Console.Error.WriteLine(e);
